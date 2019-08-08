171c18a8aa79bf1ab4ae5c4676f7862b29e18ed9
Krisztián Gesztiványi <qxy@qxy.hu>
2019-08-08 12:28:09 +0200

    Update AssemblyInfo

--- Runn-R/Properties/AssemblyInfo.cs
+++ Runn-R/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Simple command line tool to select random file and open with default program")]
 [assembly: AssemblyCompany("innerspace Research")]
 [assembly: AssemblyProduct("Runn-R")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("(c) 2019, QXY^iR")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
