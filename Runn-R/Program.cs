0ddf2789c991487414c4480d6c49b345f0cdf517
Krisztián Gesztiványi <qxy@qxy.hu>
2019-08-08 12:27:32 +0200

    Add new feature: allow rename the executable
    - executable name not hardcoded anymore
    - protect infinite execution with Mutex
    - some code refactoring & add comments

--- Runn-R/Program.cs
+++ Runn-R/Program.cs
@@ -1,29 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
+using System.Threading;
 
 namespace Runn_R
 {
 	class Program
 	{
-		static List<String> allFiles = new List<String>();
+		private static Mutex mutex = null;
+		private static bool createdNew = false;
+		private static string exeName;
+
+		private static List<String> allFiles = new List<String>();
 
 		public static void Main(string[] args)
 		{
-			bool debug = false; // default normal mode
-			if (args.Length > 0 && args[0].Equals("-D"))
-			{
-				debug = true; // enable debug mode
-			}
-
 			try
-			{		
+			{
+				// make sure only this instance running
+				mutex = new Mutex(true, "Runn-R", out createdNew);
+				if (!createdNew) return;
+
+				// get the filename of the executable
+				string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+				exeName = Path.GetFileName(codeBase);
+
+				// check argument
+				bool debug = false; // default normal mode
+				if (args.Length > 0 && args[0].Equals("-D"))
+				{
+					debug = true; // enable debug mode
+				}
+
+				// recursively read current directory
 				readDirTree(Directory.GetCurrentDirectory());
 
+				// any file found?
 				if (allFiles.Count > 0)
 				{
 					var rnd = new Random();
 					int idx = rnd.Next(allFiles.Count);
+					// select a random file
 					string rndFile = @allFiles[idx];
 
 					if (debug) // debug mode
@@ -42,9 +60,20 @@ namespace Runn_R
 				Console.Write("Press any key to continue . . . ");
 				Console.ReadKey(true);
 			}
+			finally
+			{
+				// wait if other instance trying to start
+				Thread.Sleep(100);
+				// clean up
+				if (createdNew)
+				{
+					mutex.ReleaseMutex();
+					mutex.Dispose();
+				}
+			}
 		}
 
-		static void readDirTree(string path)
+		private static void readDirTree(string path)
 		{
 			foreach (string dirName in Directory.GetDirectories(path))
 			{
@@ -53,7 +82,8 @@ namespace Runn_R
 
 			foreach (string fileName in Directory.GetFiles(path))
 			{
-				if (!Path.GetFileName(fileName).Equals("Runn-R.exe", StringComparison.OrdinalIgnoreCase))
+				// skip self
+				if (!Path.GetFileName(fileName).Equals(exeName, StringComparison.OrdinalIgnoreCase))
 				{
 					allFiles.Add(fileName);
 				}
